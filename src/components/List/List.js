5473d18a3601675d565bd41ff10eed11ea81923a
Ryszard Glegola <ryszard.glegola@gmail.com>
2020-12-20 14:04:28 +0100

    Move hero image path to Hero.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Hero from '../Hero/Hero.js';
 import styles from './List.scss';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+    heroImagePath: PropTypes.node,
+  }
+
+  static defaultProps = {
+    children: <p>I can do all those things whenever I want!</p>,
+  }
+  
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} heroImage={this.props.heroImagePath}/>
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
       </section>
     )
   }
